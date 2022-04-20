5acc1b3847f38612a6c61b25e7c70b36d7992175
Joyce <101890595+joycelee1919@users.noreply.github.com>
2022-04-20 03:23:29 +0100

    Update randomthoughts.py

--- randomthoughts.py
+++ randomthoughts.py
@@ -141,8 +141,13 @@ if surprise_me:
     st.write("Our top 5 recommendations for you:\n")
     
     gb = GridOptionsBuilder.from_dataframe(sorted_df)
-    gb.configure_default_column(editable=False, resizable=True,sorteable=True)
+
+    gb.configure_pagination()
+    gb.configure_side_bar()
+    gb.configure_default_column(groupable=True, value=True, enableRowGroup=True, aggFunc="sum", editable=True)
     
-    AgGrid(sorted_df, gridOptions = gb)
+    gridOptions = gb.build()
+
+    AgGrid(sorted_df, gridOptions=gridOptions, enable_enterprise_modules=True)
     
 #------------------------------
