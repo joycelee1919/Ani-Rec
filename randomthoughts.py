569bce781b819209fd7b1a3c28a81ff1e37ffe24
Joyce <101890595+joycelee1919@users.noreply.github.com>
2022-04-20 03:20:41 +0100

    Update randomthoughts.py

--- randomthoughts.py
+++ randomthoughts.py
@@ -143,6 +143,6 @@ if surprise_me:
     gb = GridOptionsBuilder.from_dataframe(sorted_df)
     gb.configure_default_column(editable=False, resizable=True,sorteable=True)
     
-    AgGrid(sorted_df)
+    AgGrid(sorted_df, gridOptions=gridOptions)
     
 #------------------------------
