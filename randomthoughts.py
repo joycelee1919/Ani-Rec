159a9e53f694bd26ef9d808cd752ccc54751725c
Joyce <101890595+joycelee1919@users.noreply.github.com>
2022-04-20 01:48:49 +0100

    Update randomthoughts.py

--- randomthoughts.py
+++ randomthoughts.py
@@ -98,7 +98,7 @@ def display_image(sorted_df):
 
 #------------------------------
 
-anime_kw = pd.read_csv("https://docs.google.com/spreadsheets/d/1e2bBx9ImdlIav45zxGWxu-jTMH3SWUmpBYGOXYSY3bE/edit#gid=839328658&single=true&output=csv", index_col=0)
+anime_kw = pd.read_csv("https://docs.google.com/spreadsheets/d/1e2bBx9ImdlIav45zxGWxu-jTMH3SWUmpBYGOXYSY3bE/edit#gid=839328658&output=csv")
 
 anime_kw_syn100 = anime_kw[(anime_kw.Synopsis.str.len() > 100)] 
 cv = CountVectorizer()
