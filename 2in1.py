1065fd2e2681d30d6f6dda8cdc40e8b1697c8733
Joyce <101890595+joycelee1919@users.noreply.github.com>
2022-04-20 07:48:23 +0100

    Update 2in1.py

--- 2in1.py
+++ 2in1.py
@@ -196,6 +196,8 @@ elif status == "Based on an anime you love 🥰": # content-based
     st.header('Content-based recommendation')
     st.write("Don't worry if you don't know the exact title, suggestions will be given :3")
     st.caption("Try entering random stuff like: hOwl'Ss m0vingggu c4stleeeeee, 5114, attack on titan season 3, garden of words, もののけ姫 and see what happens?")
+    st.code("""Known bug: Anime with IDENTICAL 'Name', 'English_name', 'Japanese_name' cannot be searched directly :( 
+            - ONE PIECE (MAL_ID) seem to be the only anime with this issue.""")
 
     #------------------------------
 
