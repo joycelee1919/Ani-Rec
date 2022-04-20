d233fd1ffacc73066cf2d606f2d281f68517096e
Joyce <101890595+joycelee1919@users.noreply.github.com>
2022-04-20 01:00:26 +0100

    Update randomthoughts.py

--- randomthoughts.py
+++ randomthoughts.py
@@ -1,30 +1,31 @@
-# Fantastic four
+import streamlit as st
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Word processing
 from sklearn.feature_extraction.text import CountVectorizer
 from rake_nltk import Rake
 import string
 import nltk
 from collections import Counter
 
-# Get images online
 from PIL import Image
 import requests
 
-# Fuzzy matching difflib 
 from difflib import get_close_matches
 
-# Similarity matching (keyword-based)
 from sklearn.metrics.pairwise import cosine_similarity
-# Other distance metric? # TFIDF? # Levenshtein etc.?
 
-#Show all columns in df w/o truncation
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_colwidth', None)
+
+#------------------------------
+
+st.title('‧͙⁺˚*･༓☾AniRec Randomthoughts✿✼:*ﾟ:༅｡')
+
+#------------------------------
 
 stpwrd = nltk.corpus.stopwords.words('english')
 
@@ -45,13 +46,12 @@ stpwrd += ['old', 'new', 'however', 'must', 'action', 'adventure', 'comedy', 'dr
 'set', 'day', 'episode', 'takes', 'aired', 'years', '1st', '2nd', '3rd',
 'dvd', 'becoming', 'go', 'part', 'season', 'upon', 'without', 'with', 'since',
 'ova', 'ona', 'face', 'make', 'come', 'seems', 'middle', 'become', 'episodes',
-'...', 'hile', 'let', '",', 'causes', 'certain', 'main', 'first', 'see', 'sees'] # exclude genres
+'...', 'hile', 'let', '",', 'causes', 'certain', 'main', 'first', 'see', 'sees'] 
 
 stpwrd += numbers
-
 stpwrd += alphabet
 
-# Images for display
+#------------------------------
 
 shounen = Image.open(requests.get("https://wallpaperaccess.com/full/39033.png", stream=True).raw) #Action, Shounen, Mecha
 family = Image.open(requests.get("https://funimation.in/wp-content/uploads/2022/04/Top-10-Anime-Similar-To-Spy-X-Family-That-You.jpg", stream=True).raw) #Slice of Life, Comedy, School
@@ -63,7 +63,6 @@ allanime = Image.open(requests.get("https://www.animationmagazine.net/wordpress/
 
 def display_image(sorted_df):
 
-    # Display image according to top genre
     first_genre = sorted_df['Genres'].iloc[0].split(',')
 
     if any(word in first_genre for word in ['Action', 'Shounen', 'Mecha']):
@@ -94,80 +93,76 @@ def display_image(sorted_df):
         plt.axis('off')
         plt.imshow(allanime) 
         plt.show()
-        
-# Import anime dataset with limited info and extracted keywords
+
+#------------------------------
+
 url='https://drive.google.com/file/d/1mFIcE3nnbeDkPbNQa74PGfYGvr7UgaTV/view?usp=sharing'
 file_id=url.split('/')[-2]
 dwn_url='https://drive.google.com/uc?id=' + file_id
 anime_kw = pd.read_csv(dwn_url)
 
-# Limiting ratings didn't work well :( (tried 6)
-# What about if we want longer synopsis?
-#anime_kw = anime_kw[(anime_kw.Synopsis.str.len() > 50)] #hmm (9214)
-anime_kw_syn100 = anime_kw[(anime_kw.Synopsis.str.len() > 100)] # quite good, keep for now (8145)
-# Initiate CountVectorizer
+anime_kw_syn100 = anime_kw[(anime_kw.Synopsis.str.len() > 100)] 
 cv = CountVectorizer()
 
-def random_thoughts():
+#------------------------------
 
-    brainstorm = input("What are you thinking right now?")
-    # adventure girl high school magic world friend 
-    # See if: the longer the merrier?
+brainstorm = st.text_input("What are you thinking right now?")
+# adventure girl high school magic world friend 
+# See if: the longer the merrier?
 
-    keywords_matrix = cv.fit_transform(anime_kw_syn100.Keywords) 
-    brainstorm_matrix = cv.transform(np.array([brainstorm])) 
+keywords_matrix = cv.fit_transform(anime_kw_syn100.Keywords) 
+brainstorm_matrix = cv.transform(np.array([brainstorm])) 
 
-    cosine_sim = cosine_similarity(keywords_matrix, brainstorm_matrix)
+cosine_sim = cosine_similarity(keywords_matrix, brainstorm_matrix)
 
-    # Use the numpy.ravel() Function to Convert a Matrix to an Array in NumPy.
-    cosine_sim_flattened = np.ravel(cosine_sim)
+# Use the numpy.ravel() Function to Convert a Matrix to an Array in NumPy.
+cosine_sim_flattened = np.ravel(cosine_sim)
 
-    # Top 5 anime with highest cosine similarity score between brainstorm vs keyword
+# Top 5 anime with highest cosine similarity score between brainstorm vs keyword
 
-    top5_index = np.argpartition(-cosine_sim_flattened, 5)
-    top5_index = top5_index[:5]
+top5_index = np.argpartition(-cosine_sim_flattened, 5)
+top5_index = top5_index[:5]
 
-    top5_simscore = np.partition(-cosine_sim_flattened, 5)
-    top5_simscore = -top5_simscore[:5]
+top5_simscore = np.partition(-cosine_sim_flattened, 5)
+top5_simscore = -top5_simscore[:5]
 
-    # Create new df for brainstorm, contains all cosine similarity comparisons
+# Create new df for brainstorm, contains all cosine similarity comparisons
 
-    brainstorm_df = anime_kw_syn100[['MAL_ID', 'Name', 'English_name', 'Japanese_name',
-                            'Genres', 'Synopsis', 'Type', 'Episodes', 
-                            'Rating', 'Polarity', 'Keywords']].copy()
+brainstorm_df = anime_kw_syn100[['MAL_ID', 'Name', 'English_name', 'Japanese_name',
+                        'Genres', 'Synopsis', 'Type', 'Episodes', 
+                        'Rating', 'Polarity', 'Keywords']].copy()
 
-    # New column for similarity score for all anime
+# New column for similarity score for all anime
 
-    brainstorm_df['Similarity_score'] = pd.Series(cosine_sim_flattened)
-    brainstorm_df['Similarity_score'] = brainstorm_df['Similarity_score'].apply(lambda x:round(x,3)) # 3 dp not so precise in display
+brainstorm_df['Similarity_score'] = pd.Series(cosine_sim_flattened)
+brainstorm_df['Similarity_score'] = brainstorm_df['Similarity_score'].apply(lambda x:round(x,3)) # 3 dp not so precise in display
 
-    # destructuring for later processing (bad practice but )
+# destructuring for later processing (bad practice but )
 
-    top1, top2, top3, top4, top5 = top5_index
+top1, top2, top3, top4, top5 = top5_index
 
-    # Double [[]] in iloc for output as dataframe
-    # Select only top 5 anime for display
+# Double [[]] in iloc for output as dataframe
+# Select only top 5 anime for display
 
-    brainstorm_df_output = pd.concat([brainstorm_df.iloc[[top1]], brainstorm_df.iloc[[top2]], 
-                        brainstorm_df.iloc[[top3]], brainstorm_df.iloc[[top4]], 
-                        brainstorm_df.iloc[[top5]]], ignore_index=True)
+brainstorm_df_output = pd.concat([brainstorm_df.iloc[[top1]], brainstorm_df.iloc[[top2]], 
+                    brainstorm_df.iloc[[top3]], brainstorm_df.iloc[[top4]], 
+                    brainstorm_df.iloc[[top5]]], ignore_index=True)
 
-    # Final output - discard Keyword
-    display_df = brainstorm_df_output.drop('Keywords', axis = 1)
+# Final output - discard Keyword
+display_df = brainstorm_df_output.drop('Keywords', axis = 1)
 
-    # Display common elements and results
+# Display common elements and results
 
-    elements = [word for word, word_count in Counter(" ".join(brainstorm_df_output["Keywords"]).split()).most_common(30) if word not in stpwrd ]
-    print("\nYou entered: " + brainstorm)
-    print("\nWe think you'd like anime with these elements: " + str(elements))
+elements = [word for word, word_count in Counter(" ".join(brainstorm_df_output["Keywords"]).split()).most_common(30) if word not in stpwrd ]
+print("\nYou entered: " + brainstorm)
+print("\nWe think you'd like anime with these elements: " + str(elements))
 
-    sorted_df = display_df.sort_values('Similarity_score', ascending = False)
+sorted_df = display_df.sort_values('Similarity_score', ascending = False)
 
-    display_image(sorted_df)
+display_image(sorted_df)
 
-    print("Our top 5 recommendations for you:\n")
+print("Our top 5 recommendations for you:\n")
 
-    display(sorted_df)
+display(sorted_df)
 
- random_thoughts
-        
+#------------------------------
